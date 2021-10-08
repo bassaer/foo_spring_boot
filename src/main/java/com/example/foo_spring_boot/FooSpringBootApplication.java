37c62476b76f670fe1a8b2d4863a3112b980dbda
Tsubasa Nakayama <app.nakayama@gmail.com>
2021-10-09 01:48:04 +0900

    init

---
+++ src/main/java/com/example/foo_spring_boot/FooSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.foo_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FooSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FooSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/foo_spring_boot/FooSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.foo_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
