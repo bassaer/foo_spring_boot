1cddf3811f6c8fe341ab7b677457060d4ce386fe
Tsubasa Nakayama <app.nakayama@gmail.com>
2021-11-13 02:54:43 +0900

    use slf4j

--- src/main/java/com/example/foo_spring_boot/repository/MainUserRepository.java
+++ src/main/java/com/example/foo_spring_boot/repository/MainUserRepository.java
@@ -3,17 +3,19 @@ package com.example.foo_spring_boot.repository;
 import com.example.foo_spring_boot.aop.LoggerAdvice.EnableLogger;
 import com.example.foo_spring_boot.model.User;
 import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
+@Slf4j
 @Component
 public class MainUserRepository implements UserRepository{
 
     @EnableLogger
     @Override
     public User find(@NonNull String id) {
-        System.out.println("MainUserRepository#find");
+        log.info("MainUserRepository#find");
         return User.builder()
             .id(id)
             .name(id.length() % 2 == 0 ? "hello" : "world")
